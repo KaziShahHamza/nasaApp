159f238d8eb3fe74b1f6091c273ecfafcdfc185e
soulfulDreamer <kazishahhamza@outlook.com>
2024-11-03 17:39:06 +0600

    nasaApp half done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./index.css";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
+import { useState } from "react";
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <Main />
+      {showModal && <SideBar />}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContents">
+        <h2>The Brutal Martin Landscape</h2>
+        <div>
+          <p>Lorem, ipsum.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error,
+            dolores.
+          </p>
+        </div>
+        <button>
+        <i className="fa-solid fa-angles-right"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
